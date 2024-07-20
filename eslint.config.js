204f1867f397e30adcccbfa8202b1811a19fd896
StyleShit <evyatar.daud@gmail.com>
2024-07-20 21:39:33 +0300

    chore: fix types in `eslint.config.js`
    Closes #25

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
 	eslint.configs.recommended,
 	...tseslint.configs.strictTypeChecked,
 	{
