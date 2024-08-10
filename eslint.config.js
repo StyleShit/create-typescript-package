86fc29d342b90a6e25cd3176f09f2a95ae602ea5
albinsabu2023 <albinsullas2023@gmail.com>
2024-08-11 03:27:35 +0530

    disallowed cosole.log

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,36 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-	eslint.configs.recommended,
-	...tseslint.configs.strictTypeChecked,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-			parserOptions: {
-				project: true,
-				tsconfigRootDir: import.meta.dirname,
-			},
-		},
-	},
-	{
-		ignores: [
-			'**/coverage/**',
-			'**/dist/**',
-			'**/node_modules/**',
-			'**/__snapshots__/**',
-		],
-	},
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+		"no-console":"off",
+      "no-restricted-syntax": [
+        "error",
+        {
+          "selector": "CallExpression[callee.object.name='console'][callee.property.name='log']",
+          "message": "Unexpected use of console.log."
+        }
+      ]
+    },
+  },
+  {
+    ignores: [
+      '**/coverage/**',
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/__snapshots__/**',
+    ],
+  }
 );
